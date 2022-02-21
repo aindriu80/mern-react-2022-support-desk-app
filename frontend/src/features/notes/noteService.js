d53e0ab2f1940aa53bcac2cbd1808299e396ab70
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-02-21 13:01:57 +0000

    Submitting a note on a ticket

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -13,8 +13,24 @@ const getNotes = async (ticketId, token) => {
   return response.data
 }
 
+// Get ticket note
+const createNote = async (noteText, ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.post(
+    API_URL + ticketId + '/notes',
+    { text: noteText },
+    config
+  )
+  return response.data
+}
+
 const notesService = {
   getNotes,
+  createNote,
 }
 
 export default notesService
